146f89a9f6e2dc6672515cab118b14ef4c4454f6
Wikcio1618 <ciolekwiktor@gmail.com>
2022-09-18 18:59:58 +0200

    Before library developement

---
+++ lib/model/hymn.dart
@@ -0,0 +1,11 @@
+class Hymn {
+  final int? id;
+  final String title;
+  final String text;
+
+  const Hymn({
+    this.id,
+    required this.title,
+    required this.text,
+  });
+}
--- lib/root_library.dart
+++ lib/root_library.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:hymnal_app/model/hymn.dart';
 
 class Library extends StatefulWidget {
   const Library({super.key});
-
-
 
   @override
   State<Library> createState() => _LibraryState();
 }
 
 class _LibraryState extends State<Library> {
+  List<Hymn> hymns = const [
+    Hymn(title: 'Cudowna boża łaska', text: 'twa, zbawiła z grzechów mnie..'),
+    Hymn(title: 'Gdy na ten świat', text: 'spoglądam wielki Boże'),
+    Hymn(title: 'Błogo mi', text: 'W Panu wieczny mam dział')
+  ];
+
   @override
   Widget build(BuildContext context) {
-    // ignore: prefer_const_literals_to_create_immutables
-    return ListView.builder(itemBuilder: (context, item){
-      if (item.isOdd) return const Divider();
+    return ListView(children: _libraryListTiles(hymns));
+  }
 
-      return const ListTile(
-        title: Text('q')
-      );
-    });
+  List<Widget> _libraryListTiles(List<Hymn> hymns) {
+    List<Widget> viewTiles = [];
+    for (var hymn in hymns) {
+      viewTiles.add(ListTile(
+        title: Text(hymn.title),
+        subtitle: Text(hymn.text),
+      ));
+    }
+    return viewTiles;
   }
 }
